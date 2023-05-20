ed97c22db84451d5cbc470272ef2529e797a4216
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-05-21 01:49:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function volume_sphere() {
     let radius = document.getElementById("radius").value;
    let volume =  (4/3) * Math.PI * Math.pow(radius, 3);
 	volume = volume.toFixed(4);
-     document.getElementById("volume").value = volumne
+     document.getElementById("volume").value = volume
     // console.log(radius);
 
     return false;
