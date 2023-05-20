b590ebea1f35e6499a0dd3dde943d244fa9aa0f2
anuj raghuwanshi <anujonline46@gmail.com>
2023-05-20 21:04:18 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
     //Write your code here
-  
+    let radius = document.getElementById("radius").value;
+    document.getElementById("volume").value =  (4/3) * Math.PI * Math.pow(radius, 3);;
+
+    // console.log(radius);
+
+
+     
+    
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
